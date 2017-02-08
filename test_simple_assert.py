ba5365e35e25e2562698dd645bf514f04eb8ab62
bnx05 <LovelyB@engagespark.com>
2017-02-08 13:15:17 +0800

    add xfail

--- test_simple_assert.py
+++ test_simple_assert.py
@@ -12,13 +12,13 @@ def test_open_engagespark_homepage():
     assert "Home" in browser.title
 
 
-# @pytest.mark.xfail(strict=True)
+@pytest.mark.xfail(strict=True)
 def test_login_link_available_in_homepage():
     browser.get("https://www.engagespark.com/")
     assert browser.find_element_by_link_text("login")
 
 
-# @pytest.mark.xfail(reason="needs wait")
+@pytest.mark.xfail(reason="needs wait")
 def test_open_login_page_from_homepage():
     browser.get("https://www.engagespark.com/")
     browser.find_element_by_link_text("login").click()
