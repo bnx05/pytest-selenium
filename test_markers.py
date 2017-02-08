b3ba21ffe6f1b85862cdbb7bd5d9c78100870749
bnx05 <LovelyB@engagespark.com>
2017-02-08 11:59:54 +0800

    add demo test for markers

---
+++ test_markers.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pytest
+from selenium import webdriver
+
+
+browser = webdriver.Firefox()
+browser.get("https://start.engagespark.com/sign-in/")
+
+
+@pytest.mark.login_test
+def test_assert_empty_login_form():
+    assert browser.find_element_by_name("login").get_attribute("value") == ""
+    assert browser.find_element_by_name("password").get_attribute("value") == ""
+
+
+@pytest.mark.login_test
+@pytest.mark.xfail(reason="button is disabled")
+def test_login_empty_fields():
+    browser.find_element_by_xpath("//button[contains(text(), 'Login')]").click()
+
+
+class TestSignupPage:
+
+    @pytest.mark.signup_test
+    @pytest.mark.xfail(reason="needs wait")
+    def test_assert_empty_signup_form(self):
+        assert browser.find_element_by_name("login").get_attribute("value") == ""
+        assert browser.find_element_by_name("password1").get_attribute("value") == ""
+
+    @pytest.mark.signup_test
+    @pytest.mark.xfail(reason="button is disabled")
+    def test_signup_empty_fields(self):
+        browser.find_element_by_xpath("//button[contains(text(), 'Sign Up')]").click()
