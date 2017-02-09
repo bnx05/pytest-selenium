75a2f0df3068e1b5e71b008de889146e3ff649ea
Marisa Quiaot <marisa.quiaot@gmail.com>
2017-02-09 16:42:46 +0800

    modify assert test

--- test_simple_assert.py
+++ test_simple_assert.py
@@ -7,20 +7,20 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 
 
-def test_open_engagespark_homepage():
+def test_assert_title_of_homepage():
     browser.get("https://www.engagespark.com/")
-    assert "Home" in browser.title
+    assert browser.title == "Home - Send and Receive Automated Call and SMS Text Campaigns."
 
 
-@pytest.mark.xfail(strict=True)
-def test_login_link_available_in_homepage():
+@pytest.mark.xfail()
+def test_assert_presence_of_login_link():
     browser.get("https://www.engagespark.com/")
     assert browser.find_element_by_link_text("login")
 
 
 @pytest.mark.xfail(reason="needs wait")
-def test_open_login_page_from_homepage():
+def test_assert_login_page():
     browser.get("https://www.engagespark.com/")
     browser.find_element_by_link_text("login").click()
-    assert "Sign In" in browser.title
+    assert browser.title == "engageSPARK - Sign In"
     assert browser.find_element_by_id("loginform")
