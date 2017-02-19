ba8d1485ff887e63e277684f5ff515637c177f70
bnx05 <LovelyB@engagespark.com>
2017-02-19 15:49:37 +0800

    add comments to explain tests

--- test_fixtures.py
+++ test_fixtures.py
@@ -9,15 +9,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
 
-'''
-Prior to running this module, set the environment variable "URL" first. Set the value to
-"https://start.engagespark.com/sign-in/" for the Login test. For the Signup test, set the value to
-"https://start.engagespark.com/sign-up/"
-'''
+# Prior to running this module, set the environment variable "URL" first.
+# Set the value to "https://start.engagespark.com/sign-in/" for the Login test.
+# For the Signup test, set the value to https://start.engagespark.com/sign-up/".
 
 
 class Base:
 
+    # This fixture contains the set up and tear down code for each test.
     @pytest.fixture(autouse=True)
     def browser_setup_and_teardown(self):
         self.browser = webdriver.Firefox()
@@ -27,6 +26,8 @@ class Base:
         self.browser.close()
         self.browser.quit()
 
+    # This fixture opens the URL set in the environment variable and waits
+    # until a form is visible before proceeding.
     @pytest.fixture()
     def open_url(self):
         self.browser.get(os.getenv("URL"))
@@ -39,36 +40,28 @@ class Base:
 
 class TestSignup(Base):
 
-    @pytest.mark.skipif("sign-up" not in os.getenv("URL"), reason="url provided is incorrect")
+    # When this test runs, it will use the 'browser_setup_and_teardown' fixture first, followed by
+    # the 'open_url' fixture before running the test itself.
     @pytest.mark.usefixtures("open_url")
-    @pytest.mark.parametrize("email, password", [
-        ("sample_email@blah.org", "password1"),
-        ("not_another_email@blah.org", "password2"),
-    ])
-    def test_signup_without_captcha(self, email, password):
+    def test_signup_without_captcha(self):
         print("Setting email for sign up")
-        self.enter_credentials(input_field="email", credential=email)
+        self.enter_credentials(input_field="email", credential="sample_email@blah.org")
         print("Setting password for sign up")
-        self.enter_credentials(input_field="password1", credential=password)
+        self.enter_credentials(input_field="password1", credential="password1")
         self.browser.find_element_by_css_selector("input[type='checkbox']").click()
         assert not self.browser.find_element_by_css_selector("#signupform button").is_enabled()
 
 
 class TestLogin(Base):
 
-    @pytest.mark.skipif("sign-in" not in os.getenv("URL"), reason="url provided is incorrect")
+    # When this test runs, it will use the 'browser_setup_and_teardown' fixture first, followed by
+    # the 'open_url' fixture before running the test itself.
     @pytest.mark.usefixtures("open_url")
-    @pytest.mark.parametrize("email, password", [
-        ("ghost_email_woo@blah.org", "password1"),
-        ("my_imaginary_email@blah.org", "password2"),
-        pytest.mark.xfail(
-            (os.getenv("VALID_EMAIL"), os.getenv("VALID_PASSWORD")))
-    ])
-    def test_login_with_inexistent_accounts(self, email, password):
+    def test_login_with_inexistent_accounts(self):
         print("Setting email for sign up")
-        self.enter_credentials(input_field="login", credential=email)
+        self.enter_credentials(input_field="login", credential="another_imaginary_email@blah.org")
         print("Setting password for sign up")
-        self.enter_credentials(input_field="password", credential=password)
+        self.enter_credentials(input_field="password", credential="password2")
         self.browser.find_element_by_css_selector("#loginform button").click()
         print("Error notification should appear")
         self.wait.until(EC.text_to_be_present_in_element((By.CLASS_NAME, "notification-message"), "The e-mail address and/or password you specified are not correct."))
