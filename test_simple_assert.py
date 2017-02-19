40632e33863b783d53ff47d5168852b9befe5eff
bnx05 <LovelyB@engagespark.com>
2017-02-19 12:38:36 +0800

    remove markers

--- test_simple_assert.py
+++ test_simple_assert.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import pytest
+import time
+
 from selenium import webdriver
 
 
 browser = webdriver.Firefox()
+browser.maximize_window()
 
 
 def test_assert_title_of_homepage():
@@ -12,15 +14,14 @@ def test_assert_title_of_homepage():
     assert browser.title == "Home - Send and Receive Automated Call and SMS Text Campaigns."
 
 
-@pytest.mark.xfail()
-def test_assert_presence_of_login_link():
+def test_assert_presence_of_login_button():
     browser.get("https://www.engagespark.com/")
     assert browser.find_element_by_link_text("login")
 
 
-@pytest.mark.xfail(reason="needs wait")
-def test_assert_login_page():
+def test_assert_availability_of_login_page():
     browser.get("https://www.engagespark.com/")
     browser.find_element_by_link_text("login").click()
+    time.sleep(10)
     assert browser.title == "engageSPARK - Sign In"
     assert browser.find_element_by_id("loginform")
